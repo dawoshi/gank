d6fb16d1bb79ef3278ee0f5d40fbf0ee00902d76
chenmutime@outlook.com <hdtchenmutime23>
2017-07-16 18:11:56 +0800

    更换文件位置

--- src/main/java/com/qings/elasticsearch/config/Config.java
+++ src/main/java/com/qings/elasticsearch/config/Config.java
@@ -1,16 +1,10 @@
-package com.qings.config;
+package com.qings.elasticsearch.config;
 
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
