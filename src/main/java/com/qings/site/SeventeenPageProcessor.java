384a04015ad64e60d6e374897e7c489350281355
chenmutime <chenmutime@outlook.com>
2017-07-17 16:02:15 +0800

    添加对ES的支持

--- src/main/java/com/qings/site/SeventeenPageProcessor.java
+++ src/main/java/com/qings/site/SeventeenPageProcessor.java
@@ -74,7 +74,7 @@ public class SeventeenPageProcessor extends SitePageProcessor implements PagePro
                     page.putField("site_name", CommonProperties.TYPE);
                     page.putField("url",select.get());
                     page.putField("title",html.getDocument().title());
-                    page.putField("created",html.getDocument().getElementsByClass("gb-final-date").eachText().get(0).replace("时间：",""));
+                    page.putField("publish",html.getDocument().getElementsByClass("gb-final-date").eachText().get(0).replace("时间：",""));
                     page.putField("author",html.getDocument().getElementsByClass("gb-final-author").eachText().get(0).replace("作者：",""));
                 }
             }
