cc3468060844d9987ea576268a776687c2b4b96e
heminming <minming.he@hdtmedia.com>
2017-08-08 18:00:30 +0800

    heros更换为不可变集合

--- src/main/java/com/qings/common/Heros.java
+++ src/main/java/com/qings/common/Heros.java
@@ -1,5 +1,7 @@
 package com.qings.common;
 
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 
 import java.util.List;
@@ -9,11 +11,10 @@ import java.util.List;
  */
 public class Heros {
 
-    public static List<String> heros = Lists.newArrayList();
+    public static final ImmutableSet<String> heros = ImmutableSet.of(
+            "花木兰",
+            "凯",
+            "蔡文姬"
+    );
 
-    static {
-        heros.add("花木兰");
-        heros.add("凯");
-        heros.add("蔡文姬");
-    }
 }
